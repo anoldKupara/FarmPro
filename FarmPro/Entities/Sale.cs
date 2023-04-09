11027044fe821c9443ec459e3604cad93b0c50cd
Anold Kupara <kuparaanold20@gmail.com>
2023-04-09 10:49:13 +0200

    added Sale model class

---
+++ FarmPro/Entities/Sale.cs
@@ -0,0 +1,12 @@
+﻿namespace FarmPro.Entities
+{
+    public class Sale
+    {
+        public int SaleID { get; set; }
+        public int AnimalID { get; set; }
+        public DateTime SaleDate { get; set; }
+        public decimal SalePrice { get; set; }
+        public string Comments { get; set; }
+        public virtual Animal Animal { get; set; }
+    }
+}
