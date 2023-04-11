d13e9ede9c37cd6f808665ae680fe9e45571b0a2
Anold Kupara <kuparaanold20@gmail.com>
2023-04-11 10:47:23 +0200

    added Vaccine model class

---
+++ FarmPro/Entities/Vaccine.cs
@@ -0,0 +1,11 @@
+﻿namespace FarmPro.Entities
+{
+    public class Vaccine
+    {
+        public int VaccineID { get; set; }
+        public string VaccineType { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public decimal PurchasePrice { get; set; }
+        public string Comments { get; set; }
+    }
+}
